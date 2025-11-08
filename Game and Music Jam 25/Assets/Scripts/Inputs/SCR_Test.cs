2282c71b2404eeda4614604d977f86fe27a22f64
Chosen1xox <xianxthomas1@gmail.com>
2025-11-08 18:30:25 -0500

    In progress

---
+++ Game and Music Jam 25/Assets/Scripts/Inputs/SCR_Test.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using 
+
+public class SCR_Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+    }
+}
