f9306afab2217453805cdb96e958cdf86a4c7aca
Bug Jones <charlesdheck@gmail.com>
2025-11-08 18:59:16 -0500

    trying to start an input

--- Game and Music Jam 25/Assets/Scripts/PlayerControls.cs
+++ Game and Music Jam 25/Assets/Scripts/PlayerControls.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
-
-public class NewMonoBehaviourScript : MonoBehaviour
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] float moveSpeed;
+
+    Rigidbody rigidBody;
+    private InputAction _move;
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
